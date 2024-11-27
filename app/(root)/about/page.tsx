e45170e3efd19300699302b6070112fc224bc7ef
Rahul Talukdar <42152385+mr-talukdar@users.noreply.github.com>
2024-11-27 22:05:04 +0530

    Fixed Infers

--- app/(root)/about/page.tsx
+++ app/(root)/about/page.tsx
@@ -1,6 +1,11 @@
 
 import React from 'react'
 
+interface  Post{
+    title:string;
+    id:number | string;
+}
+
 async function Page(){
     const response = await fetch('https://jsonplaceholder.typicode.com/posts/');
     if(!response.ok){ throw new Error('Not Found!'); }
@@ -8,7 +13,7 @@ async function Page(){
 
     return (
         <div>
-            {posts.map((post:any) =>(
+            {posts.map((post: Post ) =>(
                 <div key={post.id}>{post.id} : {post.title}</div>
             ))}
         </div>
